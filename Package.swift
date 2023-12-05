18da1b44aa6ccfd2e546cd7d3036210f54f200c6
Ryan Alfi <real.rezz@gmail.com>
2023-12-05 13:05:56 +0700

    fix target name

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SocialAuthTrial",
-            targets: ["SocialAuthLib"]),
+            targets: ["SocialAuthTrial"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.

419eb2e6f08febdb1ed46de14437b07329683055
Ryan Alfi <real.rezz@gmail.com>
2023-12-05 15:02:33 +0700

    modify Package.swift

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/facebook/facebook-ios-sdk.git", from: "16.2.1"),
-        .package(url: "https://github.com/google/GoogleSignIn-iOS.git", from: "7.0.0")
+        // .package(url: "https://github.com/facebook/facebook-ios-sdk.git", from: "16.2.1"),
+        // .package(url: "https://github.com/google/GoogleSignIn-iOS.git", from: "7.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -27,12 +27,15 @@ let package = Package(
             name: "SocialAuthFw",
             path: "SocialAuthFw.xcframework"
         )
-//        .target(
-//            name: "SocialAuth",
-//            dependencies: [
-//                .product(name: "FacebookLogin", package: "facebook-ios-sdk"),
-//                .product(name: "GoogleSignIn", package: "GoogleSignIn-iOS"),
-//                .target(name: "SocialAuthFw")
-//            ], path: "")
-    ]
+        /*
+        .target(
+            name: "SocialAuth",
+            dependencies: [
+                .product(name: "FacebookLogin", package: "facebook-ios-sdk"),
+                .product(name: "GoogleSignIn", package: "GoogleSignIn-iOS"),
+                .target(name: "SocialAuthFw")
+            ], path: "")
+         */
+    ],
+    swiftLanguageVersions: [.v5]
 )

479cadddb35e63cebe06ebc26f9230f7f59034ba
Ryan Alfi <real.rezz@gmail.com>
2023-12-05 11:57:53 +0700

    replace package

---
+++ SocialAuthFw.xcframework/ios-arm64/SocialAuthFw.framework/Headers/SocialAuthFw.h
@@ -0,0 +1,18 @@
+//
+//  SocialAuthFw.h
+//  SocialAuthFw
+//
+//  Created by Ari Fajrianda Alfi on 05/12/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SocialAuthFw.
+FOUNDATION_EXPORT double SocialAuthFwVersionNumber;
+
+//! Project version string for SocialAuthFw.
+FOUNDATION_EXPORT const unsigned char SocialAuthFwVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SocialAuthFw/PublicHeader.h>
+
+
